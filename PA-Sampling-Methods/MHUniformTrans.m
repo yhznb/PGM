d2e390a59295b1e75c9160a56124d46b06d6468a
yhznb <50665515+yhznb@users.noreply.github.com>
2020-04-07 19:53:42 +0800

    Add files via upload

---
+++ PA-Sampling-Methods/MHUniformTrans.m
@@ -0,0 +1,35 @@
+% MHUNIFORMTRANS
+%
+%  MCMC Metropolis-Hastings transition function that
+%  utilizes the uniform proposal distribution.
+%  A - The current joint assignment.  This should be
+%      updated to be the next assignment
+%  G - The network
+%  F - List of all factors
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+function A = MHUniformTrans(A, G, F)
+
+% Draw proposed new state from uniform distribution
+A_prop = ceil(rand(1, length(A)) .* G.card);
+
+p_acceptance = 0.0;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE
+% Compute acceptance probability
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+LogNewValue=LogProbOfJointAssignment(F,A_prop)
+LogOldValue=LogProbOfJointAssignment(F,A)
+diff=LogNewValue-LogOldValue
+
+p_acceptance=min(1,exp(diff))
+% Accept or reject proposal
+if rand() < p_acceptance
+    % disp('Accepted');
+    A = A_prop;
+end
+
+end
